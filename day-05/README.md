5a88544a7265efacf066550ae2c3b3237bd62d4f
Michael Sterling <1461273+sterlinm@users.noreply.github.com>
2024-12-13 15:08:37 -0800

    solved first part of day 5

--- day-05/README.md
+++ day-05/README.md
@@ -9,3 +9,18 @@
 - Read in input as a one-column varchar table
 - split into a table for the rules and a table for the updates
 - group the rules 
+
+## Tables to create
+
+### Input Tables
+
+page_ordering_rule is a table with one row for each potential page, and a list of all pages that must be printed after that page number
+
+page_ordering_rules:
+    - page_number (INT)
+    - later_pages (INT[])
+
+manual_updates:
+    - update_number (INT)
+    - update_page_index (INT)
+    - update_page_number (INT)
--- day-05/solution.sql
+++ day-05/solution.sql
@@ -7,6 +7,7 @@ FROM read_csv_auto(
     columns={input_data:VARCHAR}
 );
 
+CREATE TABLE page_ordering_rules AS
 WITH stage1 AS (
     SELECT
         input_data[:2]::INT AS page_num,
@@ -16,8 +17,66 @@ WITH stage1 AS (
 )
 SELECT
     page_num,
-    list_distinct(list(before_page))
+    list_distinct(list(before_page)) AS later_pages
 FROM stage1
 GROUP BY page_num
-ORDER BY page_num
-LIMIT 10;
+ORDER BY page_num;
+
+FROM page_ordering_rules
+LIMIT 1;
+
+CREATE TABLE manual_updates AS
+WITH stage1 AS (
+    SELECT
+        row_number() OVER () AS update_number,
+        list_transform(string_split(input_data, ','), x -> x::int) AS update_pages,
+        update_pages[CEIL(len(update_pages) / 2)::int] AS middle_page
+    FROM raw_input_data
+    WHERE ',' IN input_data
+)
+SELECT
+    update_number,
+    middle_page,
+    unnest(update_pages) AS update_page_number,
+    generate_subscripts(update_pages, 1) AS update_page_index,
+FROM stage1;
+
+WITH stage1 AS (
+    SELECT
+        u1.update_number,
+        u1.update_page_number,
+        u1.update_page_index,
+        u2.update_page_number AS preceding_page
+    FROM manual_updates u1
+    JOIN manual_updates u2 ON u1.update_number=u2.update_number AND u1.update_page_index >= u2.update_page_index
+    ORDER BY u1.update_number, u1.update_page_index, u2.update_page_index
+), stage2 AS (
+    SELECT
+        update_number,
+        update_page_number,
+        update_page_index,
+        list(preceding_page)[:-2] AS preceding_pages
+    FROM stage1
+    GROUP BY update_number, update_page_number, update_page_index
+    ORDER BY update_number, update_page_index
+),
+stage3 AS (
+    SELECT
+        s.*,
+        r.later_pages,
+        list_has_any(s.preceding_pages, r.later_pages) AS is_rule_violation
+    FROM stage2 s
+    LEFT JOIN page_ordering_rules r ON s.update_page_number=r.page_num
+),
+stage4 AS (
+SELECT
+    s.update_number,
+    MAX(s.is_rule_violation) AS has_rule_violation,
+    ANY_VALUE(u.middle_page) AS middle_page
+FROM stage3 s
+JOIN manual_updates u ON s.update_number = u.update_number
+GROUP BY s.update_number
+HAVING has_rule_violation = false
+)
+SELECT SUM(middle_page)
+FROM stage4;
